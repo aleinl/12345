707edd6f8058dd84fb10e419ca05f66d2e3e41c0
plio <19150945470@163.com>
2025-11-21 16:46:04 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -1,23 +1,29 @@
 //202510303129
 //2490249791@qq.com
 //陶磊
-#include<stdio.h>
- int main()
- {  int a;
-  int c;
-  int b;
-  int i=100;
-  while (i<=999)
-  {
-    a=i/100;
-    b= i/10%10;
-    c=i%100%10;
-    if (a*a*a+b*b*b+c*c*c==i)
-    {
-      printf("%d ",i);
-    }
-    i++;
-  }
-  return 0;
+#include <stdio.h>
 
- }
+void incrementArray(int *arr, int size) {
+    for(int i = 0; i < size; i++) {
+        (*(arr + i))++;  
+    }
+}
+
+int main() {
+    int arr[5];
+    
+    
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+   
+    incrementArray(arr, 5);
+    
+  
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
