caf2e93585ec7ad5f657c044c963fe0b9a211166
plio <19150945470@163.com>
2025-11-21 16:44:24 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -1,18 +1,24 @@
 //202510303129
 //2490249791@qq.com
 //陶磊
-#include<stdio.h>
-int main()
-{int a,b,c;
-    int n;
-    for (int n = 100; n < 999; n++)
-    {a=n/100;
-        b=n/10%10;
-        c=n%10;
-        if (a*a*a+b*b*b+c*c*c==n)
-        {
-            printf("%d ",n);    
-        }       
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    
+   
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
     }
-        
+    
+    int *p = arr;
+    for(int i = 0; i < 5; i++) {
+        *(p + i) *= 2;
+    }
+    
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
 }
