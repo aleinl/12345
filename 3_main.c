c04e9b7a5342be174ee7c31c553fd93f4d22c339
plio <19150945470@163.com>
2025-11-21 16:45:22 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -1,18 +1,20 @@
 //202510303129
 //2490249791@qq.com
 //陶磊
-#include<stdio.h>
- int main()
- {
-    int n=0;
-    scanf("%d",&n);
-    while (n>50||n%2==0||n%3==0||n%5==0)
-    { 
-      
-     printf("密钥不安全，请重新输入");
-     scanf("%d",&n);
-    }
-    printf("密钥安全，密码设置成功");
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
     
+    swap(&x, &y);
+    
+    printf("%d %d", x, y);
     return 0;
- }
+}
